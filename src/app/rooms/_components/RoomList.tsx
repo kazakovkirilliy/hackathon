97a909e5237a90bea7a2e2ce378ecd766d16a50e
dremileta <andreymileta@gmail.com>
2023-10-19 17:06:18 +0200

    feat: room list item init

--- src/app/rooms/_components/RoomList.tsx
+++ src/app/rooms/_components/RoomList.tsx
@@ -1,5 +1,22 @@
 import { FC } from 'react'
+import { RoomListItem } from '~/entities/room-list-item'
+import { Separator } from '~/components/ui/separator'
 
-export const RoomList: FC = (props) => {
-    return <div>Event List</div>
+export const RoomList: FC = ({ rooms }: { rooms: any[] }) => {
+    return (
+        <div className="flex w-full h-full flex-col">
+            {/*<h2 className="text-center">Rooms</h2>*/}
+            {/*<Separator className="shadow-md" />*/}
+
+            {rooms?.length ? (
+                rooms.map((r) => (
+                    <div className="" key={r.id}>
+                        <RoomListItem />
+                    </div>
+                ))
+            ) : (
+                <h1 className="text-center">No rooms found.</h1>
+            )}
+        </div>
+    )
 }
---
+++ src/entities/room-list-item.tsx
@@ -0,0 +1,9 @@
+export const RoomListItem = () => {
+    return (
+        <div className="w-full h-12 shadow-md flex items-center rounded-md p-3">
+            <div className="rounded-full bg-green-500 w-4 h-4 flex mr-2"></div>
+
+            <p>Karel</p>
+        </div>
+    )
+}
